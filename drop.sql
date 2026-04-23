f6934ff6079a7f73ae034dfb0ff375438a4b37b8
Stanislava <153832463+stanislavadot@users.noreply.github.com>
2026-04-23 17:47:01 +0200

    commit changes

--- drop.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE order_items;
-DROP TABLE orders;
-DROP TABLE products;
-DROP TABLE customers;
-
-SELECT * FROM customers;
-SELECT * FROM products;
-SELECT * FROM order_items;
-SELECT * FROM orders;
